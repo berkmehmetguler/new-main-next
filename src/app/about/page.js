4d9f227133bcc419552abf084809eeb421cd1f07
Berk M. Guler <berkmehmetguler@gmail.com>
2023-03-23 03:28:40 +0300

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+
+const page = () => {
+  return (
+    <div>
+      ABout PAge
+      <Navbar />
+    </div>
+  );
+};
+
+export default page;
